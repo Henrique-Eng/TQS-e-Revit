003cdef4281f244b52604840b747fa91c4d48a59
Henrique-Eng <henriquerodriguesrezende@gmail.com>
2025-02-09 18:42:08 -0300

    Start Ganhando os pilares

---
+++ Get_PILARES.py
@@ -0,0 +1,158 @@
+# file name: ./${DIR_PATH}/${FILE_NAME}
+from math import floor
+
+# REFERENCES AND IMPORTS
+# BEGIN>>>>>
+
+import clr
+import System
+import os
+import sys
+
+# <<< Python Modules >>>
+# BEGIN
+
+# Import traceback module from Iron Python
+import traceback
+import math
+
+# END
+
+# Import System Libraries
+clr.AddReference("System.Core")
+from System.Collections.Generic import List as SystemList
+
+# Import System Libraries
+import clr
+
+clr.AddReference("System.Core")
+from System import Math as SystemMath
+
+# Import Linq
+clr.ImportExtensions(System.Linq)
+
+# Import Dynamo Library Nodes - Geometry
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript import Geometry as DynamoGeometry
+
+
+# Import Dynamo Library Nodes - Core
+import clr
+
+clr.AddReference('DSCoreNodes')
+from DSCore import Math as DynamoMath
+
+clr.AddReference('DSCoreNodes')
+from DSCore import List as DynamoList
+
+clr.AddReference('DSCoreNodes')
+from DSCore import ImportExport as ImportExportNodes
+
+# Import Dynamo Library Nodes - Core
+clr.AddReference('DSCoreNodes')
+from DSCore import Color as DynamoColor
+
+# Import Dynamo Geometry Color
+# https://forum.dynamobim.com/t/geometrycolor-bygeometrycolor-inside-python/52724
+clr.AddReference('GeometryColor')
+from Modifiers import GeometryColor as DynamoGeometryColorize
+
+# Import Dynamo Library Nodes - Revit
+clr.AddReference("RevitNodes")
+import Revit as RevitNodes
+
+#ImportExport from Dynamo Library Nodes - Revit
+clr.AddReference("RevitNodes")
+import ImportExport as ImportExportNodes
+
+# Import ToDSType(bool) extension method
+clr.ImportExtensions(RevitNodes.Elements)
+clr.ImportExtensions(RevitNodes.GeometryConversion)
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# Import Revit API
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+# Import Revit User Interface API
+clr.AddReference("RevitAPIUI")
+from Autodesk.Revit.UI import *
+
+# Import Revit IFC API
+# https://forum.dynamobim.com/t/ifcexportutils/4833/7?u=ricardo_freitas
+clr.AddReference('RevitAPIIFC')
+from Autodesk.Revit.DB.IFC import *
+
+# Import Dynamo Services
+clr.AddReference('DynamoServices')
+from Dynamo import Events as DynamoEvents
+
+# Active Dynamo Workspace Path
+workspaceFullPath = DynamoEvents.ExecutionEvents.ActiveSession.CurrentWorkspacePath
+workspacePath = '\\'.join(workspaceFullPath.split('\\')[0:-1])
+
+# REFERENCES AND IMPORTS
+# END<<<<<
+
+# FUNCTIONS
+# BEGIN>>>>>
+
+# <<< Your classes and functions here >>>
+module_dir_path = r"C:\Classes Revit"
+
+sys.path.append(module_dir_path)
+from pydynamo import *
+
+# FUNCTIONS
+# END<<<<<
+
+# INPUTS AND VARIABLES DECLARATIONS
+# BEGIN>>>>>
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
+
+Path_directory_string = IN[0]
+
+result = []
+
+# INPUTS AND VARIABLES DECLARATIONS
+# END<<<<<
+
+# MAIN CODE
+# BEGIN>>>>>
+
+try:
+    errorReport = None
+
+    # start transaction
+    TransactionManager.Instance.EnsureInTransaction(doc)
+
+    # Your Code Here
+    # Get all .DXF's (Structural columns shop drawings) in the directory
+    result = FileSystem.DirectoryFromPath(Path_directory_string)
+
+
+
+    # End transaction
+    TransactionManager.Instance.TransactionTaskDone()
+
+except Exception as e:
+    # if error occurs anywhere in the process catch it
+    errorReport = traceback.format_exc()
+
+# Assign your output to the OUT variable
+if errorReport is None:
+    OUT = result
+else:
+    OUT = errorReport
+# MAIN CODE
+# END<<<<<
